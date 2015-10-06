e1952792ce5707da3153f40a49a70b0cee763f1b
Stefan Neidig <s.neidig@Daniels-iMac.fritz.box>
2015-10-06 14:25:47 +0200

    Fix typo in article attr_reader

--- lib/fastbill-automatic/article.rb
+++ lib/fastbill-automatic/article.rb
@@ -3,7 +3,7 @@ module Fastbill
     class Article < Base
 
       attr_reader :article_number, :title, :description, :unit_price, :allow_multiple, :is_addon, :currency_code,
-                  :vat_percent, :setup_fee, :subscription_interval, :subscription_number_events, :subscription_trail,
+                  :vat_percent, :setup_fee, :subscription_interval, :subscription_number_events, :subscription_trial,
                   :subscription_duration, :subscription_cancellation, :return_url_success, :return_url_cancel, :checkout_url
 
       def self.create(attributes)
